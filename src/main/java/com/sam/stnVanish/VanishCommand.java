fd336b4307d2e21f0339bb5dda0620201b268115
araasomohammed <araasomohammed@gmail.com>
2024-08-22 10:51:29 +0300

    OP is required to use command

--- src/main/java/com/sam/stnVanish/VanishCommand.java
+++ src/main/java/com/sam/stnVanish/VanishCommand.java
@@ -1,5 +1,6 @@
 package com.sam.stnVanish;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -26,6 +27,10 @@ public class VanishCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            if(!player.isOp()) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command");
+                return true;
+            }
 
             if (vanishedPlayers.contains(player.getUniqueId())) {
                 vanishedPlayers.remove(player.getUniqueId());
